2f550b15c236764affcfed40702249de79bf1a0a
david1707 <DavidAlacant@gmail.com>
2018-12-20 21:07:18 +0100

    Vue Cli

---
+++ myproject/src/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="main-nav">
+        <h2>Menu</h2>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+<style>
+nav.main-nav {
+    text-align: center;
+}
+nav ul{
+    padding: 0;
+}
+nav li {
+    display: inline-block;
+    list-style-type: none;
+    margin: 0;
+}
+</style>
