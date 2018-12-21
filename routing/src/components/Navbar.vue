1fe64ea48710677080fd941b73999c09694cf2b1
david1707 <DavidAlacant@gmail.com>
2018-12-21 14:03:49 +0100

    All routing done and finished

---
+++ routing/src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <nav class="main-nav">
+        <ul>
+            <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+            <li><router-link :to="{ name: 'About' }">About</router-link></li>
+        </ul>
+        <h2>User Profiles</h2>
+        <ul>
+            <li v-for="userId in userIds" :key="userId.id">
+                <router-link :to="{ name: 'ViewProfile', params: { user_id: userId }}"><span>Profile #{{ userId }}</span></router-link>
+            </li>
+        </ul>
+        <h2>Navigation Controls</h2>
+        <ul>
+            <li><button @click="goBack">Go to the previous page</button></li>
+            <li><button @click="goHome">Redirect to Home</button></li>
+            <li><button @click="goForward">Go to the next page</button></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            userIds: ['1', '2', '3', '4'],
+        }
+    },
+    methods: {
+        goHome() {
+            this.$router.push( { name: 'Home' });
+        },
+        goBack() {
+            this.$router.go(-1);
+        },
+        goForward() {
+            this.$router.go(+1);
+        },
+    }
+}
+</script>
+
+<style>
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+a {
+    color: #42b983;
+}
+li {
+    display: inline-block;
+    margin: 10px;
+}
+</style>
--- routing/src/router/index.js
+++ routing/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import About from '@/components/About'
+import ViewProfile from '@/components/ViewProfile'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/profile/:user_id',
+      name: 'ViewProfile',
+      component: ViewProfile
+    },
   ]
 })
