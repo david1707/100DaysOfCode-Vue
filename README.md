b8d542fe1d96b6fd32702f633f13dae1d78738e3
david1707 <DavidAlacant@gmail.com>
2018-12-23 12:24:31 +0100

    Welcome login and chat templates created

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-### Day 04 lessons
+### Day 05 lessons
 
+- Real-time-chat with Vue and Firebase
+
+## How to use it
+
+· git clone this project with git clone URL
+· Rename firebase/your-init.js to firebase/init.js
+· Create a firebase project and copy your project info there
+· Run the server with npm run dev
---
+++ real-time-chat/src/components/Chat.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container chat">
+        <div class="center teal-text">Ninja Chat</div>
+        <div class="card">
+            <div class="card-content">
+                <ul class="messages">
+                    <li>
+                        <span class="teal-text">Name</span>
+                        <span class="grey-text text-darken-3">Message</span>
+                        <span class="grey-text time">11-11-2011</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-action">
+                <input type="text" >
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Chat',
+    props: ['name'],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+
+<style>
+.chat h2{
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
+.chat span {
+    font-size: 1.4em;
+}
+.chat .time {
+    display: block;
+    font-size: 1.2em;
+}
+</style>
