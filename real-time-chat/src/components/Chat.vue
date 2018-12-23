5056eac4a57df83f321957dd34c2b2b6a06557a8
david1707 <DavidAlacant@gmail.com>
2018-12-23 13:34:34 +0100

    Chat finished

--- real-time-chat/src/components/Chat.vue
+++ real-time-chat/src/components/Chat.vue
@@ -3,32 +3,56 @@
         <div class="center teal-text">Ninja Chat</div>
         <div class="card">
             <div class="card-content">
-                <ul class="messages">
-                    <li>
-                        <span class="teal-text">Name</span>
-                        <span class="grey-text text-darken-3">Message</span>
-                        <span class="grey-text time">11-11-2011</span>
+                <ul class="messages" v-chat-scroll>
+                    <li v-for="message in messages" :key="message.id">
+                        <span class="teal-text">{{ message.name }}</span>
+                        <span class="grey-text time">[{{ message.timestamp }}]</span>
+                        <span class="grey-text message-text text-darken-3">{{ message.content }}</span>
                     </li>
                 </ul>
             </div>
             <div class="card-action">
-                <input type="text" >
+                <NewMessage :name="name" />
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import NewMessage from '@/components/NewMessage';
+import db from '@/firebase/init';
+import moment from 'moment';
+
 export default {
     name: 'Chat',
     props: ['name'],
+    components: {
+        NewMessage
+    },
     data() {
         return {
-
+            messages: [],
         }
     },
     methods: {
 
+    },
+    created() {
+        let ref = db.collection('messages').orderBy('timestamp');
+
+        ref.onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type == "added") {
+                    let doc = change.doc;
+                    this.messages.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        content: doc.data().content,
+                        timestamp: moment(doc.data().timestamp).format('LLL'),
+                    });
+                }
+            });
+        })
     }
 }
 </script>
@@ -43,7 +67,25 @@ export default {
     font-size: 1.4em;
 }
 .chat .time {
+    font-size: 0.9em;
+}
+.chat .message-text {
     display: block;
     font-size: 1.2em;
+}
+.messages {
+    max-height: 300px;
+    overflow: auto;
+}
+.messages::-webkit-scrollbar {
+  width: 3px;
+}
+ 
+.messages::-webkit-scrollbar-track {
+  background: #ddd;
+}
+ 
+.messages::-webkit-scrollbar-thumb {
+  background: #aaa; 
 }
 </style>
