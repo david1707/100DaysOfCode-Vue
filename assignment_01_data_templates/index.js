f233a987cc48626eb42eca4771b9edfffbc9fa91
david1707 <DavidAlacant@gmail.com>
2018-12-27 12:41:30 +0100

    Finished theory for today

--- assignment_01_data_templates/index.js
+++
@@ -1,9 +0,0 @@
-new Vue({
-    el: '#exercise',
-    data: {
-        name: 'David',
-        age: 34,
-        randomNumber: Math.random(),
-        imageUrl: 'https://pbs.twimg.com/profile_images/875996174305472512/upM71pVR_400x400.jpg',
-    }
-})
