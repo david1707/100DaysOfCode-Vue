88ab3542c7b77e73e44bc287e245099b3555c1e1
david1707 <DavidAlacant@gmail.com>
2019-01-11 18:19:04 +0100

    Start day-24

--- 30-days-of-vue/directives/test.js
+++ 30-days-of-vue/directives/test.js
@@ -2,13 +2,18 @@ var app = new Vue({
     el: '#app',
     data: {
         greeting: 'Hello World!',
-        user: 'Hassan Djirdeh',
-        city: 'Toronto',
+        alertMessage: '',
     },
     methods: {
         changeGreeting() {
             this.greeting = this.greeting === 'Hello World!' ? 'What is up' : 'Hello World!';
             console.log(this.$data);
+        },
+        alertMessageGreeting() {
+            this.alertMessage = 'You typed something!';
+        },
+        alertEnterGreeting() {
+            this.alertMessage = 'You typed something!';
         }
     },
 })
