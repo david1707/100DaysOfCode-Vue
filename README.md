597adf3321abe58d0057f4ecf289a38b3dc945e4
david1707 <DavidAlacant@gmail.com>
2019-02-06 21:18:18 +0100

    Created About page and almost done

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 - Submit button dissabled if there is any error at the form
 - Added icons from MaterialIcons to the form
 - Now it sends the information (as a mock. I need an email sender service)
+- Fixed 100vh images (somewhat)
 
 ### TODO
+- Add Google Maps address: https://developers.google.com/maps/documentation/javascript/adding-a-google-map
 - Fix navbar for mobile phones
-- Fix 100vh images
+
--- animal_shelter/src/views/About.vue
+++ animal_shelter/src/views/About.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="about-us">
-    <h1 class="center-align">This is the About Us page</h1>
+    <div>
+      <img src="https://c.pxhere.com/photos/74/ea/greece_dog_sleep_tired_hot_santorini-1021532.jpg!d" alt="">
+    </div>
+    <div class="container">
+      <div class="row">
+        <h3>What we do</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore dignissimos, molestias blanditiis repellat non itaque pariatur omnis nisi dolorum maxime facilis praesentium quisquam. Praesentium ut dolores molestias consectetur odit.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, autem, at cupiditate laudantium nulla ducimus, commodi veniam corporis voluptatum dolor error tempore eos harum molestias optio distinctio rem dolores mollitia porro est. Enim minus aperiam natus!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam ut reprehenderit minima? Aliquid aut quibusdam necessitatibus ad ipsa quod esse eius itaque rerum. Iure, omnis. Aliquam, maxime nihil?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta ut repellat fugit repudiandae quos recusandae cupiditate quo voluptates impedit beatae eum totam itaque ipsa, nulla voluptatum laborum.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur labore, soluta architecto vero sed, fugiat nobis omnis placeat inventore quibusdam minus dicta maiores. Sapiente doloribus eligendi provident alias. Ratione consequuntur placeat vitae soluta in nihil pariatur.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati!</p>
+      </div>
+      <div class="row">
+        <h3>Why we do it</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ab repellendus a voluptates excepturi molestias explicabo laborum natus adipisci dolor, quod consequatur! Perferendis illum provident totam?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel eos culpa quis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vel! Ab deleniti, quisquam dolorem beatae perferendis adipisci, sed, doloribus velit nobis dolores optio ea voluptatem! Blanditiis, velit molestiae eveniet aut minima consectetur?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati doloribus natus nostrum nulla qui dolore velit quod aliquid dolores. Illo est sit ab labore officia aspernatur harum aut.</p>
+      </div>
+      <div class="row">
+        <h3>Our achievements:</h3>
+        <ul>
+          <li>- Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic quos voluptatem, tempore fugiat eveniet veritatis ratione?</li>
+          <li>- Animi, laborum officia ut cumque fuga vel et architecto qui voluptate repellendus! Consequuntur quisquam ducimus quo?</li>
+          <li>- Velit excepturi reprehenderit amet similique nemo repellendus. Amet, sequi aperiam sed maiores saepe eos praesentium odit.</li>
+          <li>- Unde sint earum cupiditate, harum eius ab porro neque corrupti quam nostrum totam expedita deserunt. Quisquam.</li>
+          <li>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptas ad porro consectetur! Nulla ipsum optio veritatis quia ad animi!</li>
+        </ul>
+      </div>
+      <div class="row">
+        <h3>Where we are:</h3>
+
+      </div>
+      <div class="row"></div>
+    </div>
   </div>
 </template>
 
@@ -11,3 +46,14 @@ export default {
 }
 </script>
 
+<style lang="scss">
+  .about-us > div > img {
+    width: 100vw;
+    height: 99vh;
+  }
+
+  .row > ul > li {
+    padding: 0.5em;
+  }
+
+</style>
